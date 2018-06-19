05167e794884619dd4d6da7143f8136e3b15973e
Matt Hickman <matthickman@HickeyBook-Pro.local>
2018-06-18 20:58:16 -0700

    Added Google Maps pod

--- Pinz App/AppDelegate.swift
+++ Pinz App/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Firebase
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        GMSServices.provideAPIKey("AIzaSyDSpP745C_dQt3HYU_qxHwnQr4UoeoIjww")
+        GMSPlacesClient.provideAPIKey("AIzaSyDSpP745C_dQt3HYU_qxHwnQr4UoeoIjww")
         return true
     }
 
---
+++ Pinz App/View Controllers/MapViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MapViewController.swift
+//  Pinz App
+//
+//  Created by Matt Hickman on 6/18/18.
+//  Copyright © 2018 Matt Hickman. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func loadView() {
+        // Create a GMSCameraPosition
+        let camera = GMSCameraPosition.camera(withLatitude: 45.5122, longitude: -122.6587, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 45.5122, longitude: -122.6587)
+        marker.title = "Portland"
+        marker.snippet = "Oregon"
+        marker.map = mapView
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
